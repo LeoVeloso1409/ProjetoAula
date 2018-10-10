1a8dc556489e4966d84e9fc3174c0d605fa6d72e
LeoVeloso1409 <neves.lv@gmail.com>
2018-10-09 22:02:21 -0300

    Edicao das Classes(PrincipalController.java, ProdutoCadastro.fxml, ProdutoCadastroController.java) configuraçao do botao Salvar; criação do metodo de abrir nova tela;

--- src/ui/main/PrincipalController.java
+++ src/ui/main/PrincipalController.java
@@ -5,9 +5,18 @@
  */
 package ui.main;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -23,5 +32,37 @@ public class PrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    private void abrirJanelaCadastro(ActionEvent event) throws IOException{
+        
+        // Abrir a janela de Cadastro de Produtos
+        //Código para criar uma nova janela
+        
+        //Leitura do FXML
+        Parent root = FXMLLoader.load(getClass().getResource("/ui/produto/cadastro/ProdutoCadastro.fxml"));
+
+        //Criação da cena
+        Scene scene = new Scene(root);
+
+        //Criação do Stage (window)
+        //Configurar o tipo da janela que vai abrir
+        Stage stage = new Stage(StageStyle.DECORATED);
+        stage.setResizable(false);
+        stage.setTitle("Cadastro de Produto");
+        
+        //Icone
+        stage.getIcons().add(new Image("/imagens/icone.png"));
+        
+        //Adicionando a scene no stage
+        stage.setScene(scene);
+        
+        //Modality para bloquear outras janelas
+        stage.initModality(Modality.APPLICATION_MODAL);    
+        
+        //Mostrando a nova janela
+        stage.show();
+        
+    }
+            
     
 }
